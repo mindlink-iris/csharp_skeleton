29d5693a7ff3f073d1b5894ca5c1811592ba2269
Iris Esener <Iris.Esener@mindlinksoft.com>
2026-09-15 15:14:40 +0100

    Test initialization position

--- WakingSkeleton.Test/RoverInitialization.cs
+++ WakingSkeleton.Test/RoverInitialization.cs
@@ -13,16 +13,27 @@ public class Tests
         Assert.That(rover.Heading, Is.EqualTo(Heading.North));
     }
 
+    [Test]
+    public void InitializePosition()
+    {
+        var rover = new Rover(new Position(2, 3), Heading.North);
+        Assert.That(rover.Position, Is.EqualTo(new Position(2, 3)));
+    }
+
     private class Rover
     {
         private Heading heading;
+        private Position position;
 
         public Rover(Position position, Heading heading)
         {
             this.heading = heading;
+            this.position = position;
         }
 
         public Heading Heading => this.heading;
+
+        public Position Position => this.position;
     }
 
     public enum Heading
@@ -33,8 +44,25 @@ public class Tests
 
     private class Position
     {
+        private int x;
+        private int y;
+
         public Position(int x, int y)
         {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int X => this.x;
+        public int Y => this.y;
+
+       public override bool Equals(object? other)
+        {
+            if (other is Position otherPosition)
+            {
+                return this.x == otherPosition.x && this.y == otherPosition.y;
+            }
+            return false;
         }
     }
 }
